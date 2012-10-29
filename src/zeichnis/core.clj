4439dd4ed0d2988c98bce55593b0347fbbe83c44
Gabriel Pickard <gabriel@whitepaperanalytics.com>
2012-10-29 22:10:47 +0800

    non-working in-between shit

--- src/zeichnis/core.clj
+++ src/zeichnis/core.clj
@@ -1,9 +1,79 @@
 (ns zeichnis.core
   (:require [clojure.set :as cset]))
 
-(defmulti resolve-node :store-id
-  "Node accessors take the form {:store-id _ :node-id _}
-   where :store-id and :node-id can be arbitrary serializable terms or tokens.")
+(def databases (atom {}))
+(def datastores (atom {}))
+
+(defn get-database* [args]
+  (@databases (:db (:input args))))
+
+(defn z [args]
+  (db-z (get-database* args) args))
+
+(defprotocol IZeichnisPeer
+  "A ZeichnisPeer manages connections to a set of storage layers, allowing to distribute operations and overlay results."
+  (ds-op [this op ds args]))
+
+
+(deftype ZeichnisPeer [datastores]
+  IZeichnisPeer
+  (ds-op [op ds args]
+    (send (@datastores ds) op args)))
+
+
+;; {:function function-id :input args}  }
+
+;; {:input .  :function . :output . }
+
+;; db-objects come with some means of assigning functions for operation specifications
+
+;; database objects then (if they are objects) have to somehow find the specific code
+;; for that operation and that bucket (buckets are the agnostic term for nodes belonging to one indexing-scheme)
+;; that function gets called with the database thingy as argument, so the signature is
+;; (function db args)
+;; which speaks to the storage backends via the peer (the peer just acts as a proxy for the
+;; datastores
+
+(defprotocol IDatabase
+  (z-op [this args])
+  (db-op [this args]))
+
+(deftype SingleStoreDB [peer datastore-conf actions-conf]
+  IDatabase
+  (z-op [this args]
+    (let [action (:function (actions-conf (args :function)))]
+      (action this args)))
+  (db-op [this args]
+    ;; here we need to do our action for all datastores
+    ;; and we need some sort of synchronization / error handling
+    ;; best for now to go with one datastore only then we don' have to worry about it
+    ;; but the approach is to store the information about what db-ops correspond
+    ;; to which ds-ops with which args transformation in datastores-conf
+    ;; :)
+    (ds-op peer)))
+
+(defmacro zrn [db bucket content]
+  `(z {:function :resolve-node :input {:bucket ~bucket :content ~content :db ~db}}))
+
+(defn db-insert [db args]
+  (let [bucket (:bucket args)
+        db (:db args)
+        root (zrn db bucket '_)]
+    ))
+
+;; interface:
+;; terms, metadata, partial orders, find-children, (find-parents), has?
+;; there is internal and external metadata
+;; internal metadata is exposed to the user
+;; external metadata is used by the storage system to do its job
+;; mark whether something is stored
+;; do the whole indexing part (so index is metadata)
+;; so indexing is an implementation detail (well the ability to ask certain queries depends on it).
+
+;; todo: figure out how to do graphs as term-like datastructure units
+;; got an idea to go via nodes for equivalent structures and have them split up into different actually
+;; atomically present and realized partitions
+
 
 (defprotocol BlobStore
   "Basic key-value storage API for Zeichnis.
@@ -42,11 +112,10 @@
       nil)))
 
 (extend-type clojure.lang.Symbol
+;; Here we implement subsumption for symbols.
+;; It is assumed that a symbol will subsume any term, simply by sybstituting to that term.
   Subsumer
   (subsume
-    "Here we implement subsumption for symbols.
-
-     It is assumed that a symbol will subsume any term, simply by sybstituting to that term."
     ([this term]
        (subsume this term []))
     ([this term path-prefix]
@@ -56,7 +125,7 @@
 
 (defn subsume-keys [prototype term parent-keys child-keys path-prefix]
   "For a structural element with keys (so either map or sequence), compute substitutions recursively."
-  (if (or (= parent-keys child-keys) (cset/subset?  parent-keys child-keys))
+  (when (or (= parent-keys child-keys) (cset/subset?  parent-keys child-keys))
     (let [ext-keys (cset/difference (set child-keys) (set parent-keys))
           exts (zipmap (map conj (repeat path-prefix) ext-keys)
                        (map get (repeat term) ext-keys))
@@ -74,6 +143,20 @@
             nil
             (merge exts substs)))))
 
+(defprotocol Keyable
+  (subs-keys [this]
+    "Returns keys of substructures."))
+
+(extend-type clojure.lang.Sequential
+  Keyable
+  (subs-keys [this]
+    (range (count this))))
+
+(extend-type clojure.lang.IPersistentMap
+  Keyable
+  (subs-keys [this]
+    (keys this)))
+
 (extend-type clojure.lang.IPersistentMap
   Subsumer 
  (subsume
@@ -81,9 +164,7 @@
        (subsume this term []))
     ([this term path-prefix]
        (let [parent-keys (keys this)
-             child-keys (if (map? term)
-                          (keys term)
-                          (range (count term)))]
+             child-keys (subs-keys term)]
          (subsume-keys this term parent-keys child-keys path-prefix)))))
 
 ;;todo add extra path
@@ -119,7 +200,7 @@
   (add-link-in* [this label source-node] [this label source-node annotation])
   (add-link-out* [this label target-node] [this label target-node annotation])
   (follow-links [this label] [this label annotation])
-  ())
+  (reload [this]))
 
 ;; i'm not quite happy with the way links are done here.
 ;; would prefer to do link adding external to nodes
@@ -137,13 +218,16 @@
     "Creates a new node representing the supplied content and returns it,
      integrating it into the indexing scheme (e.g. partial order)."))
 
-(defprotocol PartialOrder
- ; (all> [this item])
-  (any> [this item])
- ; (all< [this item])
- ; (any< [this item])
- ; (has? [this item])
-  )
+(defn follow-transitive )
+
+(defn descend [& nodes]
+  (mapcat #(follow-links :down %) nodes))
+(defn ascend [& nodes]
+   (mapcat #(follow-links :up %) nodes))
+
+(defn descend-to [content & nodes]
+  (loop []))
+(defn ascend-to [content & nodes])
 
 
 ;; how about making everything callback- and update-based?
