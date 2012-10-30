130a3fb29afb9edaf361a1466b3c77ff30e1d371
Gabriel Pickard <gabriel@whitepaperanalytics.com>
2012-10-30 18:30:50 +0800

    zwischenstand schmischenstand

--- src/zeichnis/core.clj
+++ src/zeichnis/core.clj
@@ -1,46 +1,70 @@
 (ns zeichnis.core
   (:require [clojure.set :as cset]))
 
-(def databases (atom {}))
-(def datastores (atom {}))
+(def default-peer (atom nil))
 
-(defn get-database* [args]
-  (@databases (:db (:input args))))
-
-(defn z [args]
-  (db-z (get-database* args) args))
+(defn z
+  ([args] (z @default-peer args))
+  ([peer args]
+     (peer-z peer args)))
 
 (defprotocol IZeichnisPeer
-  "A ZeichnisPeer manages connections to a set of storage layers, allowing to distribute operations and overlay results."
-  (ds-op [this op ds args]))
-
-
-(deftype ZeichnisPeer [datastores]
-  IZeichnisPeer
-  (ds-op [op ds args]
-    (send (@datastores ds) op args)))
-
-
-;; {:function function-id :input args}  }
-
-;; {:input .  :function . :output . }
-
-;; db-objects come with some means of assigning functions for operation specifications
-
-;; database objects then (if they are objects) have to somehow find the specific code
-;; for that operation and that bucket (buckets are the agnostic term for nodes belonging to one indexing-scheme)
-;; that function gets called with the database thingy as argument, so the signature is
-;; (function db args)
-;; which speaks to the storage backends via the peer (the peer just acts as a proxy for the
-;; datastores
+  "A ZeichnisPeer manages connections to a set of storage layers and logical databases, allowing to distribute operations and overlay results."
+  (peer-z [this args]))
 
 (defprotocol IDatabase
-  (z-op [this args])
+  "A database recieves requests, runs these as actions and translates calls from these actions to db-op into calls to the storage backend."
+  (db-z [this args])
   (db-op [this args]))
 
-(deftype SingleStoreDB [peer datastore-conf actions-conf]
+(defprotocol IDatastore
+  (ds-op [this args]))
+
+(deftype ZeichnisPeer [databases]
+  IZeichnisPeer
+  (peer-z [this args]
+    ;; potentially here we could filter for some special non-db functions
+    (db-z (databases (:db args)) args)))
+
+(defn fmap [f m]
+  "Update all keys in map"
+  (into (empty m) (for [[k v] m] [k (f v)])))
+
+;; these are init multimethods for datastore and database config maps
+(defmulti init-ds :type)
+(defmulti init-db #(:type (first %)))
+
+;; do i by default want to pass in the different actions?
+;; the action set should be extensible
+;; databases can redefine actions
+;; datastores can as well
+;; so in many cases we probably just re-distribute the same operations to
+;; all datastores
+
+(defmethod init-db :default [conf dss]
+  ((:type conf) (dss (:datastore (:conf conf)))))
+(defmethod init-ds :default [conf]
+  ((:type conf) conf))
+
+(defn init-peer [database-conf datastore-conf]
+  (let [dss (fmap init-ds datastore-conf)
+        dbs (fmap init-db database-conf)]
+    (ZeichnisPeer dbs)))
+
+(defn init-default-peer
+  ([db-conf ds-conf]
+     (init-default-peer db-conf ds-conf false))
+  ([db-conf ds-conf hard?]
+     (if (or hard? (nil? @default-peer)) ; this is not entirely safe, but it's just meant to catch the stupid anyway
+       (reset! default-peer (init-peer db-conf ds-conf))
+       (throw (Exception. "Default peer should not be initialized more than once, try update-peer")))))
+
+;;;;; from here downward starts the wasteland
+
+
+(deftype SingleStoreDB [datastore-conf actions-conf]
   IDatabase
-  (z-op [this args]
+  (db-z [this peer args]
     (let [action (:function (actions-conf (args :function)))]
       (action this args)))
   (db-op [this args]
